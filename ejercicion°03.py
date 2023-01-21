e74f8a7f63e968e12e8565d23d64ae555e6c8042
Kevin Quispe <121902039+KevinQuispeCarpio@users.noreply.github.com>
2023-01-21 08:55:07 -0500

    Update ejercicion°03.py

--- ejercicion°03.py
+++ ejercicion°03.py
@@ -1,17 +1,14 @@
 #  17.3 Calcular la diferencia simétrica entre 3 conjuntos
 conjunto1 = {1, 2, 3, 4, 5}
 conjunto2 = {0, 2, 7}
-conjunto3 = {2, 8, 10, 15}
-# Imprimos los 3 conjuntos
+# Imprimos los 2 conjunto
 print('A = ', conjunto1)
 print('B = ', conjunto2)
-print('C = ', conjunto3)
-# Unimos los 3 conjuntos
-a = conjunto1 & conjunto2 & conjunto3
+# Unimos los 2 conjuntos
+a = conjunto1 & conjunto2 
 for i in a:
     conjunto1.discard(i)
     conjunto2.discard(i)
-    conjunto3.discard(i)
-# Hallamos la diferencia de los 3 conjuntos
-print('La diferencia simétrica de los 3 conjuntos es:',
-      (conjunto1 | conjunto2 | conjunto3))
+# Hallamos la diferencia simétrica de los 2 conjuntos
+print('La diferencia simétrica de los 2 conjuntos es:',
+      (conjunto1 | conjunto2)
